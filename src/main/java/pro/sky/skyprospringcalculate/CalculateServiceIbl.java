4b8b190c78aea9dddd911b0cd8d8cf9ddaef9f82
Evgeniy <Safronovbr@mail.ru>
2024-02-08 19:31:06 +0300

    commit2

--- src/main/java/pro/sky/skyprospringcalculate/CalculateServiceIbl.java
+++ src/main/java/pro/sky/skyprospringcalculate/CalculateServiceIbl.java
@@ -11,7 +11,25 @@ public class CalculateServiceIbl implements CalculateService {
 
     @Override
     public String sum(int num1, int num2) {
+
         return num1 + " + " + num2 + " = " + String.valueOf(num1 + num2);
+    }
+
+    @Override
+    public String minus(int num1, int num2) {
+        return num1 + " - " + num2 + " = " + String.valueOf(num1 - num2);
+    }
+
+    @Override
+    public String multiply(int num1, int num2) {
+        return num1 + " * " + num2 + " = " + String.valueOf(num1 * num2);
+    }
+
+    @Override
+    public String divide(double num1, double num2) {
+        if (num2 != 0) {
+            return (int) num1 + " / " + (int) num2 + " = " + String.valueOf(num1 / num2);
+        } else return "Eroro you dolbaeb";
     }
 
 }
